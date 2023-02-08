96cbb669182457592373c1fe1276dbd3b4a99719
Vitor Claudio <51538799+vitorclaudio@users.noreply.github.com>
2023-02-08 01:21:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function CleaningService() {
             <ul>
               <li><a href="#services">Services</a></li>
               <li><a href="#about">About</a></li>
-              <li><a href="#contatct">Contact</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
           </nav>
         </header>
